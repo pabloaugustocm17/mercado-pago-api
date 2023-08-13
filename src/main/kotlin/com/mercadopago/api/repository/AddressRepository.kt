d49405ffb7be686f0b4d5fbeaeed501539e08bc9
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-08-13 20:30:41 -0300

    feat: repository address

---
+++ src/main/kotlin/com/mercadopago/api/repository/AddressRepository.kt
@@ -0,0 +1,10 @@
+package com.mercadopago.api.repository
+
+import com.mercadopago.api.models.Address
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface AddressRepository : JpaRepository<Address, UUID> {
+}
