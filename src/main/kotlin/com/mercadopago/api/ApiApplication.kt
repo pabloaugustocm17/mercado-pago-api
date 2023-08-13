7747c930659c2e0d757cf01ef7383f4e323cbd2a
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-08-13 20:29:56 -0300

    fix: add annotations

--- src/main/kotlin/com/mercadopago/api/ApiApplication.kt
+++ src/main/kotlin/com/mercadopago/api/ApiApplication.kt
@@ -1,9 +1,13 @@
 package com.mercadopago.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EntityScan("com.mercadopago.api")
+@EnableJpaRepositories(basePackages = ["com.mercadopago.api.repository"])
 class ApiApplication
 
 fun main(args: Array<String>) {
