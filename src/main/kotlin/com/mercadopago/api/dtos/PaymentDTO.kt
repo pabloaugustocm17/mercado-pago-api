dd278a05490088c7e5f1e13eb383d04102027301
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-08-15 09:52:14 -0300

    fix: refactor request

--- src/main/kotlin/com/mercadopago/api/dtos/PaymentDTO.kt
+++ src/main/kotlin/com/mercadopago/api/dtos/PaymentDTO.kt
@@ -1,8 +1,9 @@
 package com.mercadopago.api.dtos
 
+import java.util.UUID
+
 data class PaymentDTO(
         val paymentMethod : PaymentMethodDTO,
-        val products : ArrayList<ProductDTO>,
-        val user : UserDTO,
-        val address : AddressDTO
+        val products : ArrayList<UUID>,
+        val user : UUID
 )
