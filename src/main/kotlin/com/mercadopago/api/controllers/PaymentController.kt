b3607476a740a929ebd6026c2862069b0e03e6a1
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-08-15 10:23:08 -0300

    fix: adaptation parameters

--- src/main/kotlin/com/mercadopago/api/controllers/PaymentController.kt
+++ src/main/kotlin/com/mercadopago/api/controllers/PaymentController.kt
@@ -2,6 +2,8 @@ package com.mercadopago.api.controllers
 
 import com.mercadopago.api.dtos.PaymentDTO
 import com.mercadopago.api.services.PaymentService
+import com.mercadopago.api.services.ProductService
+import com.mercadopago.api.services.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,14 +12,22 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api/v1/paymentmethods/")
 @RestController
-class PaymentController(var paymentService: PaymentService) {
+class PaymentController(
+        var paymentService: PaymentService,
+        var productService : ProductService,
+        var userService: UserService,
+) {
 
     @PostMapping
     fun realizePayment(
             @RequestBody dto : PaymentDTO
     ) : ResponseEntity<Any> {
 
-        val payment = paymentService.realizePayment(dto)
+        val user = userService.getUserById(dto.user)
+
+        val products = productService.getProductsByIds(dto.products)
+
+        val payment = paymentService.realizePayment(dto.paymentMethod, user, products)
 
         return ResponseEntity.ok(payment)
     }
